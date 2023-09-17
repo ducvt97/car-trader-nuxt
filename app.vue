5aebf7763dbe9ff40f06bc9d0f2891e2ec44fe56
ducvt97 <ducvt97@gmail.com>
2023-09-18 00:43:16 +0700

    Init project

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- Header -->
+    <header class="sticky w-full h-16 top-0 left-0 flex items-center z-50">
+      CarTrader
+    </header>
+    <!-- Home Hero -->
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative
+    w-full
+    h-screen
+    bg-[url('https://www.lucidmotors.com/transform/90e54da1-47ff-40f4-b832-d4ed436f5df5/')]
+    bg-no-repeat bg-cover bg-bottom"
+  ></div>
+</template>
